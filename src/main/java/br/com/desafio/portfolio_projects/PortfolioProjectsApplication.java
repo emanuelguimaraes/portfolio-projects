db8db50f5ce57f1755dea0a5b018ee8b0a8fba71
Emanuel Brito <emanuelguimaraes23@gmail.com>
2024-11-14 21:08:10 -0300

    Implementação da estrutura inicial do projeto

---
+++ src/main/java/br/com/desafio/portfolio_projects/PortfolioProjectsApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafio.portfolio_projects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioProjectsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/desafio/portfolio_projects/PortfolioProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafio.portfolio_projects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
