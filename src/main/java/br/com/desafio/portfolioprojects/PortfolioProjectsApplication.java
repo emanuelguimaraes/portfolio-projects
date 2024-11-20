6e766274b91ff34d93571bae5759c3afb033bda0
Emanuel Brito <emanuelguimaraes23@gmail.com>
2024-11-20 06:26:36 -0300

    Inclusão do processo de build e execução da aplicação atráves de Docker e docker compose

--- src/main/java/br/com/desafio/portfolioprojects/PortfolioProjectsApplication.java
+++ src/main/java/br/com/desafio/portfolioprojects/PortfolioProjectsApplication.java
@@ -2,12 +2,18 @@ package br.com.desafio.portfolioprojects;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PortfolioProjectsApplication {
+public class PortfolioProjectsApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(PortfolioProjectsApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioProjectsApplication.class, args);
 	}
-
 }
